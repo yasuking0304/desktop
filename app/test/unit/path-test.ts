3b53575bf5d7faae8b7702ff7910a9ed927308c5
Markus Olsson <j.markus.olsson@gmail.com>
2025-03-24 11:59:47 +0100

    Don't use bare expect: Fix faulty expected value

--- app/test/unit/path-test.ts
+++ app/test/unit/path-test.ts
@@ -28,9 +28,9 @@ describe('path', () => {
     if (__DARWIN__ || __LINUX__) {
       it('encodes spaces and hashes', () => {
         const dirName =
-          '/Users/The Kong #2\\AppData\\Local\\GitHubDesktop\\app-1.0.4\\resources\\app'
+          '/Users/The Kong #2/AppData/Local/GitHubDesktop/app-1.0.4/resources/app'
         const uri = encodePathAsUrl(dirName, 'index.html')
-        expect(uri.startsWith('file:////Users/The%20Kong%20%232/'))
+        expect(uri.startsWith('file:///Users/The%20Kong%20%232/')).toBeTrue()
       })
     }
   })
