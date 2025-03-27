f688751cbde497f0232ed89230d85ee138673afd
Markus Olsson <j.markus.olsson@gmail.com>
2025-03-27 16:15:45 +0100

    Well, i hate this

--- app/test/helpers/temp.ts
+++ app/test/helpers/temp.ts
@@ -1,15 +1,26 @@
-/* eslint-disable no-sync */
-
-/** Module for creating and managing temporary directories and files, using the
- * `temp` Node module
- */
 import { mkdtemp, rm } from 'fs/promises'
 import { tmpdir } from 'os'
 import { join } from 'path'
 import { TestContext } from 'node:test'
+import { sleep } from '../../src/lib/promise'
+import { isErrnoException } from '../../src/lib/errno-exception'
+
+const codes = ['EMFILE', 'ENFILE', 'EBUSY']
+
+// Reimplementation of retry logic in rimraf:
+// https://github.com/isaacs/rimraf/blob/8733d4c30078a1ae5f18bb6affe83c1eea0259b4/src/retry-busy.ts#L10
+const clean = async (path: string) => {
+  for (let i = 1; i <= 6; i++) {
+    await rm(path, { recursive: true }).catch((e: unknown) =>
+      isErrnoException(e) && codes.includes(e.code)
+        ? sleep(Math.ceil(Math.pow(i, 1.2)))
+        : Promise.reject(e)
+    )
+  }
+}
 
 export const createTempDirectory = (t: TestContext) =>
   mkdtemp(join(tmpdir(), 'desktop-test-')).then(path => {
-    t.after(() => rm(path, { recursive: true }))
+    t.after(() => clean(path))
     return path
   })
