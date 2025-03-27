bfb2c6ebc8715d1c8266280f60708e5f54b729ec
Markus Olsson <j.markus.olsson@gmail.com>
2025-03-27 08:41:06 +0100

    :art: cleanup

--- script/test.mjs
+++ script/test.mjs
@@ -36,11 +36,8 @@ const files =
 
 // I would _looooove_ to use the `--env-file` option, but it doesn't override
 // existing environment variables and we need to override some of them.
-Object.entries(
-  parseEnv(await readFile(join(import.meta.dirname, '..', '.test.env'), 'utf8'))
-).forEach(([k, v]) => {
-  process.env[k] = v
-})
+const testEnv = parseEnv(await readFile(join(projectRoot, '.test.env'), 'utf8'))
+Object.entries(testEnv).forEach(([k, v]) => (process.env[k] = v))
 
 const args = [
   '--disable-warning=ExperimentalWarning',
