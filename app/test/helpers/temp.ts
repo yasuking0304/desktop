d2a2b2719ca3b4e9021fca0b3515b2762a195b93
Markus Olsson <j.markus.olsson@gmail.com>
2025-03-27 16:16:16 +0100

    inline it

--- app/test/helpers/temp.ts
+++ app/test/helpers/temp.ts
@@ -5,14 +5,12 @@ import { TestContext } from 'node:test'
 import { sleep } from '../../src/lib/promise'
 import { isErrnoException } from '../../src/lib/errno-exception'
 
-const codes = ['EMFILE', 'ENFILE', 'EBUSY']
-
 // Reimplementation of retry logic in rimraf:
 // https://github.com/isaacs/rimraf/blob/8733d4c30078a1ae5f18bb6affe83c1eea0259b4/src/retry-busy.ts#L10
 const clean = async (path: string) => {
   for (let i = 1; i <= 6; i++) {
     await rm(path, { recursive: true }).catch((e: unknown) =>
-      isErrnoException(e) && codes.includes(e.code)
+      isErrnoException(e) && ['EMFILE', 'ENFILE', 'EBUSY'].includes(e.code)
         ? sleep(Math.ceil(Math.pow(i, 1.2)))
         : Promise.reject(e)
     )
