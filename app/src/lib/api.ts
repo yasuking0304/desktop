ab88c08b49caeab53cebcc432f98550aa49aa7c1
tidy-dev <tidy-dev@github.com>
2025-04-22 13:54:48 -0400

    Add method to create push protection bypass
    Introduced a new method `createPushProtectionBypass` in the API class to create a push protection bypass for a repository. This includes sending a POST request to the GitHub API with a reason and placeholder ID, and handling errors with appropriate logging.

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -814,6 +814,12 @@ interface IAPIAliveWebSocket {
 
 type TokenInvalidatedCallback = (endpoint: string, token: string) => void
 
+interface IAPICreatePushProtectionBypassResponse {
+  reason: string
+  expire_at: string
+  token_type: string
+}
+
 /**
  * An object for making authenticated requests to the GitHub API
  */
@@ -2136,6 +2142,48 @@ export class API {
         e
       )
       throw e
+    }
+  }
+
+  /**
+   * Creates a push protection bypass for a repository.
+   *
+   * This method sends a POST request to the GitHub API to create a bypass
+   * for push protection in a specified repository. The bypass is associated
+   * with a reason and a placeholder ID.
+   *
+   * @param owner - The owner of the repository.
+   * @param name - The name of the repository.
+   * @param reason - The reason for creating the bypass - false_positive, used_in_tests, will_fix_later.
+   * @param placeholderId - The placeholder ID associated with the bypass.
+   * @param bypassURL - The URL to retry the bypass creation on Github.com in case of failure.
+   * @returns A promise that resolves to the response of the bypass creation.
+   * @throws An error if the bypass creation fails, including a warning log.
+   */
+  public async createPushProtectionBypass(
+    owner: string,
+    name: string,
+    reason: string,
+    placeholderId: string,
+    bypassURL: string
+  ): Promise<IAPICreatePushProtectionBypassResponse> {
+    const path = `repos/${owner}/${name}/secret-scanning/push-protection-bypasses`
+    const body = {
+      reason,
+      placeholder_id: placeholderId,
+    }
+
+    try {
+      const response = await this.ghRequest('POST', path, { body })
+      return await parsedResponse<IAPICreatePushProtectionBypassResponse>(
+        response
+      )
+    } catch (e) {
+      const msg = `Unable to create push protection bypass for ${owner}/${name} 
+      with reason ${reason} and 
+      placeholderId ${placeholderId}. Try again at: ${bypassURL}`
+      log.warn(msg, e)
+      throw new Error(msg)
     }
   }
 }
