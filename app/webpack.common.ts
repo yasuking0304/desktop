1c340de972ee2bf559bbcada0fd3a83b24d4704a
Sergio Padrino <sergio.padrino@gmail.com>
2026-01-29 14:27:54 +0100

    (WIP) Make vscode-jsonrpc node version work (instead of browser)

--- app/webpack.common.ts
+++ app/webpack.common.ts
@@ -84,6 +84,15 @@ export const renderer = merge({}, commonConfig, {
       template: path.join(__dirname, 'static', 'index.html'),
       chunks: ['renderer'],
     }),
+    new webpack.NormalModuleReplacementPlugin(/^vscode-jsonrpc$/, resource => {
+      resource.request = 'vscode-jsonrpc/lib/node/main.js'
+    }),
+    new webpack.NormalModuleReplacementPlugin(
+      /vscode-jsonrpc[\\/]node\.js$/,
+      resource => {
+        resource.request = 'vscode-jsonrpc/lib/node/main.js'
+      }
+    ),
     new webpack.DefinePlugin(
       Object.assign({}, replacements, {
         __PROCESS_KIND__: JSON.stringify('ui'),
@@ -141,17 +150,15 @@ export const highlighter = merge({}, commonConfig, {
         modes: {
           enforce: true,
           name: (mod: any) => {
-            const builtInMode =
-              /node_modules[\\\/]codemirror[\\\/]mode[\\\/](\w+)[\\\/]/i.exec(
-                mod.resource
-              )
+            const builtInMode = /node_modules[\\\/]codemirror[\\\/]mode[\\\/](\w+)[\\\/]/i.exec(
+              mod.resource
+            )
             if (builtInMode) {
               return `mode/${builtInMode[1]}`
             }
-            const external =
-              /node_modules[\\\/]codemirror-mode-(\w+)[\\\/]/i.exec(
-                mod.resource
-              )
+            const external = /node_modules[\\\/]codemirror-mode-(\w+)[\\\/]/i.exec(
+              mod.resource
+            )
             if (external) {
               return `ext/${external[1]}`
             }
